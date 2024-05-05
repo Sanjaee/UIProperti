1e103ffd273ce4f81c58dfca0fe40a6023ee1528
Sanjaee <afrizaahmad18@gmail.com>
2024-05-05 13:05:24 +0700

    category maintance

--- src/Components/CardProperti.jsx
+++ src/Components/CardProperti.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const CardProperti = () => {
-  const [Data, setData] = useState([]);
+  const [data, setData] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,35 +14,68 @@ const CardProperti = () => {
     fetchData();
   }, []);
 
+  const filterByCategory = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const filteredData = selectedCategory
+    ? data.filter((item) => item.category === selectedCategory)
+    : data;
+
   return (
-    // grid
-    <div className="w-full grid grid-cols-1  justify-center  px-6 py-6">
-      {/* card pproperti */}
-      {Data.map((item) => (
-        <Link to={`/detail/${item.id}`} key={item.id}>
-          <div
-            key={item.id}
-            className=" w-full  rounded-lg border-[1px] border-[#D9D9D9] shadow-md mb-4"
-          >
-            <img
-              className="object-cover h-[296.400px] w-full rounded-lg"
-              src={item.thumbnail}
-              alt=""
-            />
-            <div className="flex flex-col p-2">
-              <p className="text-[#212121] text-[18px] font-semibold">
-                {" "}
-                {item.title}
-              </p>
-              <p className="text-[#6D7588] text-[18px]">{item.brand}</p>
-              <p className="text-[#212121] text-[18px]  font-bold">
-                {item.price}
-              </p>
+    <>
+      <div className="w-full z-50  sticky  top-12 h-16 items-center flex px-4 bg-white  overflow-x-auto">
+        <button
+          className="mr-4 px-4 py-2 rounded-md bg-gray-200"
+          onClick={() => filterByCategory("")}
+        >
+          All
+        </button>
+        <button
+          className="mr-4 px-4 py-2 rounded-md bg-gray-200"
+          onClick={() => filterByCategory("fragrances")}
+        >
+          Fragrances
+        </button>
+        <button
+          className="mr-4 px-4 py-2 rounded-md bg-gray-200"
+          onClick={() => filterByCategory("laptops")}
+        >
+          Laptops
+        </button>
+        <button
+          className="mr-4 px-4 py-2 rounded-md bg-gray-200"
+          onClick={() => filterByCategory("smartphones")}
+        >
+          Smartphones
+        </button>
+      </div>{" "}
+      <div className="w-full grid grid-cols-1 justify-center px-6 py-6">
+        {filteredData.map((item) => (
+          <Link to={`/detail/${item.id}`} key={item.id}>
+            <div
+              key={item.id}
+              className="w-full rounded-lg border-[1px] border-[#D9D9D9] shadow-md mb-4"
+            >
+              <img
+                className="object-cover h-[296.400px] w-full rounded-lg"
+                src={item.thumbnail}
+                alt=""
+              />
+              <div className="flex flex-col p-2">
+                <p className="text-[#212121] text-[18px] font-semibold">
+                  {item.title}
+                </p>
+                <p className="text-[#6D7588] text-[18px]">{item.brand}</p>
+                <p className="text-[#212121] text-[18px] font-bold">
+                  {item.price}
+                </p>
+              </div>
             </div>
-          </div>
-        </Link>
-      ))}
-    </div>
+          </Link>
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,14 +1,12 @@
 import SliderTop from "../../Components/SliderTop";
 import Navbar from "../../Components/Navbar";
 import CardProperti from "../../Components/CardProperti";
-import CategoryProperti from "../../Components/CategoryProperti";
 
 const HomePage = () => {
   return (
     <>
       <Navbar />
       <SliderTop />
-      <CategoryProperti />
       <CardProperti />
     </>
   );
